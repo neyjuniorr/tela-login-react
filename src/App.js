a4c8d77038cb8f5cb5994af2be6623f2511fe001
neyjuniorr <118519647+neyjuniorr@users.noreply.github.com>
2023-07-03 13:25:19 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+       
+      <h1>ÁREA DO ALUNO</h1>
+      <form>
+        <input type="text" placeholder="Digite aqui seu melhor e-mail" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
+
+
+
---
+++ src/page2.js
@@ -0,0 +1,11 @@
+function PaginaInicial() {
+    return (
+      <div>
+        <h2>Página Inicial</h2>
+        {/* Conteúdo da página */}
+      </div>
+    );
+  }
+ 
+  
+  export default PaginaInicial;
